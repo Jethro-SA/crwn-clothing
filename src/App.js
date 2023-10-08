cd51134fbc912d79b7bf057083e8d66568fededa
JNMSolutions <46893081+JNMSolutions@users.noreply.github.com>
2023-10-08 20:09:57 +0100

    added cart context, shopping icon and cart dropdown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import Shop from "./components/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 
 const App = () => {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,22 @@
 import { Link, Outlet } from "react-router-dom";
 import "./navigation.styles.scss";
 import { Fragment, useContext } from "react";
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
-
   const { currentUser } = useContext(UserContext);
+  const { cartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrwnLogo className="logo"/>
+          <CrwnLogo className="logo" />
         </Link>
 
         <div className="nav-links-container">
@@ -21,22 +24,20 @@ const Navigation = () => {
             SHOP
           </Link>
 
-          {
-            currentUser ? (
-              <Link className="nav-link" onClick={signOutUser} to="/">
-                SIGN OUT
-              </Link>
-            ) :
-            (
-              <Link className="nav-link" to="/auth">
-                SIGN IN
-              </Link>
-            )
-          }
-
-          
+          {currentUser ? (
+            <Link className="nav-link" onClick={signOutUser} to="/">
+              SIGN OUT
+            </Link>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
         </div>
       </div>
+
+      {cartOpen && <CartDropdown />}
 
       <Outlet />
     </Fragment>
